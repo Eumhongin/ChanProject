618ed7c79bf461a27bb72671e090a3776f0da8fb
Eumhongin <zizon0wpxm@naver.com>
2018-11-05 16:02:15 +0900

    123

--- Survey/dlstjxm.php
+++ Survey/dlstjxm.php
@@ -1,16 +1,19 @@
 <?php
+
 $arr_length = $_POST['length'];
-$question = array_fill(0,$arr_length,NULL);
-for($i = 0;$i<$arr_length;$i++){
-  $question[$i] = $_POST['question'.$i];
-  echo "$question[$i]";
-}
+$question = array_fill(0,$arr_length,NULL); //길이가 arr_length인 배열 선언
 
 $Univ = $_POST['Univ'];
 $Major = $_POST['Major'];
 $cat = count($question);
-echo "$Univ";
-echo "$Major";
-echo "$cat";
+echo "대학교는$Univ<br />";
+echo "학과는$Major<br />";
+echo "배열의 길이는$cat<br />";
+
+for($i = 0;$i<$arr_length;$i++){
+  $question[$i] = $_POST['question'.$i];
+  echo "질문 $i 는 $question[$i]<br />";
+}
+
 
  ?>
--- Survey/sub_src/survey.js
+++ Survey/sub_src/survey.js
@@ -1,61 +1,57 @@
+$(document).ready(function() {
 
-
-$(document).ready(function(){
-
-if($(window).outerWidth() >= '2560'){
+  if ($(window).outerWidth() >= '2560') {
     $('.Sub_Back').css({
-      'height':$(window).outerHeight()*0.4+'px'
+      'height': $(window).outerHeight() * 0.4 + 'px'
     });
 
-  }else if($(window).outerWidth() >= '1024'){
+  } else if ($(window).outerWidth() >= '1024') {
     $('.Sub_Back').css({
-      'height':$(window).outerHeight()*0.3+'px'
+      'height': $(window).outerHeight() * 0.3 + 'px'
     });
-  }else if($(window).outerWidth() >= '768'){
+  } else if ($(window).outerWidth() >= '768') {
     $('.Sub_Back').css({
-      'height':$(window).outerHeight()*0.2+'px'
+      'height': $(window).outerHeight() * 0.2 + 'px'
     });
-  }else if($(window).outerWidth() >= '0'){
+  } else if ($(window).outerWidth() >= '0') {
     $('.Sub_Back').css({
-      'height':$(window).outerHeight()*0.1+'px'
+      'height': $(window).outerHeight() * 0.1 + 'px'
     });
   }
   $('input').css({
-    'height':$('.survey_wrap').outerHeight()*0.05+'px'
+    'height': $('.survey_wrap').outerHeight() * 0.05 + 'px'
   });
-
-
-
 
   $('.plus_btn > button').css({
-    'height':$('.plus_btn > button').outerWidth()+'px'
+    'height': $('.plus_btn > button').outerWidth() + 'px'
   });
   $('.submit_btn > button').css({
-    'height':$('.submit_btn > button').outerWidth()*0.3+'px'
+    'height': $('.submit_btn > button').outerWidth() * 0.3 + 'px'
   });
   $('.survey_wrap').css({
-    'height':$('.survey_content').outerHeight()*1.2+'px'
+    'height': $('.survey_content').outerHeight() * 1.2 + 'px'
   });
 });
-function content_adding(number){
-  let changeint = 1+number;
+
+function content_adding(number) {
+  let changeint = 1 + number;
 
   let content = $('<div>').addClass('content').appendTo('.content_control');
   let content_main = $('<div>').addClass('content_main').appendTo(content);
   let content_main_p = $('<p>').text('면접 질문').appendTo(content_main);
   let content_content = $('<div>').addClass('content_content row').appendTo(content_main);
-  let content_content_input = $('<input type="text" name="question'+changeint+'" value="">').appendTo(content_content);
+  let content_content_input = $('<input type="text" name="question' + changeint + '" value="">').appendTo(content_content);
   let delete_content = $('<div>').addClass('delete_content').appendTo(content_content);
-  let delete_content_btn = $('<button type="button" name="button" onclick="content_delete(this)">').appendTo(delete_content);
+  let delete_content_btn = $('<button type="button" name="button" onclick="content_delete(this,' + changeint + ')">').appendTo(delete_content);
   console.log($('.content').outerHeight());
   $(content).css({
-    'height':$('.content').outerHeight()+'px'
+    'height': $('.content').outerHeight() + 'px'
   });
   $(content_content_input).css({
-    'height':$('.content_main > input').outerHeight()+'px'
+    'height': $('.content_main > input').outerHeight() + 'px'
   });
   $(delete_content).css({
-    'margin-left':parseInt($('.content_content').outerWidth()-$('.content_content > input').outerWidth()-$('.delete_content').outerWidth())*0.8+'px'
+    'margin-left': parseInt($('.content_content').outerWidth() - $('.content_content > input').outerWidth() - $('.delete_content').outerWidth()) * 0.8 + 'px'
   });
 
   $(delete_content_btn).css({
@@ -64,38 +60,53 @@ function content_adding(number){
     'background-image': 'url("../img/delete_btn.png")',
     'background-repeat': 'no-repeat',
     'background-size': 'cover',
-    'width':$('.content_main > input').outerHeight()+'px',
-    'height':$('.content_main > input').outerHeight()+'px'
+    'width': $('.content_main > input').outerHeight() + 'px',
+    'height': $('.content_main > input').outerHeight() + 'px'
   });
-  console.log('survey height : '+ $('.survey_wrap').outerHeight());
-  console.log('content : '+$(content).outerHeight());
+
   $('.survey_wrap').css({
-    'height':$('.survey_wrap').outerHeight()+$(content).outerHeight()+'px'
+    'height': $(content).offset().top + $('.plus_btn').outerHeight() + $('.submit_btn').outerHeight() + 'px'
   });
 
   //   $('.plus_btn > button').remove();
   //   $('<button type="submit" name="button" onclick="content_adding('+1+changeint+')">').css(btn_css).appendTo('.plus_btn');
   //$(window).scrollTop();
-  console.log($(content).offset().top);
-  let v = $(content).outerHeight()*changeint;
-  console.log(changeint);
-  $('.checklength').attr({'value':1+parseInt($('.checklength').val())});
-  console.log('value : '+$('.checklength').val());
-  $('.plus_btn > button').attr({'onclick' : 'content_adding('+changeint+')'});
-  $('html, body').animate({scrollTop : v}, 400);
+
+  let v = $(content).outerHeight() * changeint * 2;
+
+  $('.checklength').attr({
+    'value': 1 + parseInt($('.checklength').val())
+  });
+
+  $('.plus_btn > button').attr({
+    'onclick': 'content_adding(' + changeint + ')'
+  });
+  $('html, body').animate({
+    scrollTop: v
+  }, 400);
 };
-function content_delete(e){
+function content_delete(e, number) {
   //$(this).parents('content').remove();
-  console.log($('.content_main > p').css('margin'));
-  console.log($('.survey_wrap').outerHeight());
-  console.log($(e).parents('.content').outerHeight());
-  console.log($('.survey_wrap').outerHeight()-$(e).parents('.content').outerHeight());
+  // console.log($('.content_main > p').css('margin'));
+  // console.log($('.survey_wrap').outerHeight());
+  // console.log($(e).parents('.content').outerHeight());
+  // console.log($('.survey_wrap').outerHeight() - $(e).parents('.content').outerHeight());
+  // $('.survey_wrap').css({
+  //   'height':$('.survey_wrap').outerHeight()-($(e).parents('.content').outerHeight()+parseInt($(e).parents('.content_main').children('p').css('margin-top'))*2)+'px'
+  // });
+
+  let sub = number-1;
+  console.log(sub);
   $('.survey_wrap').css({
-    'height':$('.survey_wrap').outerHeight()-($(e).parents('.content').outerHeight()+parseInt($(e).parents('.content_main').children('p').css('margin-top'))*2)+'px'
+    'height': $('.content').last().offset().top + $('.plus_btn').outerHeight() + $('.submit_btn').outerHeight() - $('.content').last().outerHeight() + 'px'
   });
   $(e).parents('.content').remove();
-  $('.checklength').attr({'value':$('.checklength').val()-1});
-  console.log('value : '+$('.checklength').val());
-
+  $('.checklength').attr({
+    'value': $('.checklength').val() - 1
+  });
+  console.log('value : ' + $('.checklength').val());
+  $('.plus_btn > button').attr({
+    'onclick': 'content_adding('+sub+')'
+  });
 
 };
